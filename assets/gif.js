5b1239c55c128630df93bf7d6dd9d633e9b8da48
Matthew Taube <taubemj@gmail.com>
2019-03-23 18:05:10 -0400

    working links

--- assets/gif.js
+++ assets/gif.js
@@ -13,15 +13,16 @@ $(document).ready(function () {
         for (var i = 0; i < topics.length; i++) {
 
             var button = $("<button>");
-            button.addclass(topic);
+            button.addClass("topic");
             button.text(topics[i]);
             button.attr("data-naruto", topics[i]);
             $("#buttons-view").append(button);
         }
     }
 
-    $("#buttons-view").on("click", function () {
+    $("#buttons-view").on("click", ".topic", function () {
         var name = $(this).attr("data-naruto");
+        console.log(name);
         var myQuery = "https://api.giphy.com/v1/gifs/search?q=" +
             name + "&api_key=CTfBXVmDVk9OqaXdHblJlwpVylFwlFll&limit=10&rating=pg";
 
@@ -33,21 +34,48 @@ $(document).ready(function () {
             .then(function (response) {
 
                 var results = response.data;
+                console.log(results);
 
-                for (var i = 0; i < results.length; i++)
+                for (var i = 0; i < results.length; i++) {
 
                     var x = $("<div>");
-                var p = $("<p>").text("Rating: " + results[i].rating);
-                var narutoImage = $("<img>");
-                narutoImage.attr("src", results[i].images.fixed_height.url);
-                x.append(p);
-                x.append(narutoImage);
-                $("#gifs-here").prepend(x);
+                    x.addClass("gifs")
+                    var p = $("<p>").text("Rating: " + results[i].rating);
+                    var narutoImage = $("<img>");
+                    narutoImage.addClass("gify")
+                    narutoImage.attr("data-state", "still")
+                    narutoImage.attr("data-still", results[i].images.fixed_height_still.url)
+                    narutoImage.attr("data-animate", results[i].images.fixed_height.url)
+                    narutoImage.attr("src", results[i].images.fixed_height_still.url);
+                    x.append(p);
+                    x.append(narutoImage);
+                    $("#gifs-here").prepend(x);
 
 
-
+                }
             })
+
     })
+
+    $("#add-name").on("click", function (event) {
+        event.preventDefault();
+        var name = $("#name-input").val().trim();
+        topics.push(name);
+        $("#name-input").val("")
+        renderButtons()
+
+    })
+
+    $("#gifs-here").on("click",".gify", function () {
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    });
 
     renderButtons()
 
